a4e3cde9cb7b011f9d0d12f451c4db04c1a84f1f
Monicaasensio <Monicaasensio@yahoo.com>
2024-02-27 13:53:55 -0500

    nav and footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Footer.css'
+import { Link } from 'react-router-dom';
+import Logo from './Assets/logohappypantry.png';
+import instagram_icon from './Assets/instagram-icon.webp'
+import facebook_icon from './Assets/facebook-icon.png'
+import twitter_icon from './Assets/youtube-icon.png'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-section">
+        <div className='footer-logo'>
+            <Link to="/"><img src={Logo} alt="Happy Pantry logo" /></Link>
+        </div>
+        <div className="footer-socials">
+        <div className="footer-icons">
+            <img src={instagram_icon} alt="instagram" />
+        </div>
+        <div className="footer-icons">
+            <img src={facebook_icon} alt="facebook" />
+        </div>
+        <div className="footer-icons">
+            <img src={twitter_icon} alt="twitter" />
+        </div>
+      </div>
+        <div className="footer-links">
+        <h2>Links</h2>
+      <ul>
+        <li><Link style={{textDecoration: 'none', color: '#FB7300'}} to="/">About Us</Link></li>
+        <li><Link style={{textDecoration: 'none', color: '#FB7300'}} to="/Event">Volunteer</Link></li>
+        <li><Link style={{textDecoration: 'none', color: '#FB7300'}} to="/">FAQs</Link></li>    
+        <li><Link style={{textDecoration: 'none', color: '#FB7300'}} to="/Map">Find A Pantry</Link></li>    
+        <li><Link style={{textDecoration: 'none', color: '#FB7300'}} to="/Account">Keep Up With Us</Link></li>    
+      </ul>
+      </div>
+      </div>
+    <div className="footer-copyright">
+        <hr />
+        <p>Copyright @ 2024 - All Rights Reserved</p>
+    </div>
+    </div>
+  )
+}
