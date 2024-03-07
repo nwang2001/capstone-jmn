c8f3900061819e2d64c64c1f413af65775fdd41e
Nikki Wang <nikkiw011201@gmail.com>
2024-03-07 14:45:21 -0500

    nikki stuff

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -4,18 +4,25 @@ import { useNavigate } from 'react-router-dom';
 import '../Components/account.css';
 
 export default function Account() {
-    const [firstName, setFirstName] = useState('');
+    // const [firstName, setFirstName] = useState('');
     const navigate = useNavigate();
 
-    useEffect(() => {
-        axios.get('http://localhost:3500/users/firstname')
-            .then(response => {
-                setFirstName(response.data.firstName);
-            })
-            .catch(error => {
-                console.error('Error fetching user data:', error);
-            });
-    }, []);
+    const firstName = ({ user }) => (
+        <div>
+            <h2>Personal Information</h2>
+            <p>Name: {user.firstName}</p>
+        </div>
+    );
+
+    // useEffect(() => {
+    //     axios.get('http://localhost:3500/users/firstname')
+    //         .then(response => {
+    //             setFirstName(response.data.firstName);
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching user data:', error);
+    //         });
+    // }, []);
 
     const handleSignOut = () => {
         localStorage.clear();
@@ -26,6 +33,7 @@ export default function Account() {
     return (
         <div className='account'>
             <h1>Welcome to your account! </h1>
+            {firstName}
             <div >
                 <button className="sign-out-btn" onClick={handleSignOut}>Sign Out</button>
             </div>
